149fd5cd1ff24be734ddcef8e66fde60a3141369
Slozzyondul <solomonondula@gmail.com>
2024-06-27 11:22:29 +0300

    adding set state to the foating button to collect the input  string data

--- flutter_projects/task_manager_app/task_manager/lib/pages/home_page.dart
+++ flutter_projects/task_manager_app/task_manager/lib/pages/home_page.dart
@@ -12,12 +12,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
+
+  String? _newTaskContent;
+
   //constructor for the homepage class
   _HomePageState();
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+    print("input value: $_newTaskContent");
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: _deviceHeight * 0.15,
@@ -65,12 +69,29 @@ class _HomePageState extends State<HomePage> {
 
   Widget _addTaskButton() {
     return FloatingActionButton(
-      onPressed: () {
-        //print("button pressed");
-      },
+      onPressed: _displayTaskPopup,
       child: const Icon(
         Icons.add,
       ),
+    );
+  }
+
+  void _displayTaskPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext _context) {
+        return AlertDialog(
+          title: const Text("Add Task"),
+          content: TextField(
+            onSubmitted: (_value) {},
+            onChanged: (_value) {
+              setState(() {
+                _newTaskContent = _value;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
