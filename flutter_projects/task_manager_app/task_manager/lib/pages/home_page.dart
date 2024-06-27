b57829e06df3d0584873ad846c2a0eac3b2bb63c
Slozzyondul <solomonondula@gmail.com>
2024-06-27 08:37:25 +0300

    creating stateful widget for the app homepage

---
+++ flutter_projects/task_manager_app/task_manager/lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  HomePage();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+class _HomePageState extends State<HomePage> {
+  //constructor for the homepage class
+  _HomePageState();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
