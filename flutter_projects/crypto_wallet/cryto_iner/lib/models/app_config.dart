c9c7f07ebae66a38c9976bc90c9df102c41bc699
Slozzyondul <solomonondula@gmail.com>
2024-06-28 17:24:13 +0300

    creating http service and dio for api

--- flutter_projects/crypto_wallet/cryto_iner/lib/models/app_config.dart
+++ flutter_projects/crypto_wallet/cryto_iner/lib/models/app_config.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
 class AppConfig {
-  String COIN_API_BASE_URL;
-  String? COIN_API_KEY;
+  String COIN_API_KEY;
+  AppConfig({required this.COIN_API_KEY});
 
-  AppConfig({required this.COIN_API_BASE_URL, required this.COIN_API_KEY});
 }
