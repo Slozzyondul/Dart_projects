48106a7b4387044ca09117e54a7b7ede8a0d1b24
Slozzyondul <solomonondula@gmail.com>
2024-06-28 19:02:09 +0300

    adding a coin dropdown menu button

--- flutter_projects/crypto_wallet/cryto_iner/lib/pages/home_page.dart
+++ flutter_projects/crypto_wallet/cryto_iner/lib/pages/home_page.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: no_leading_underscores_for_local_identifiers
 
-class HomePage extends StatefulWidget{
+import 'package:cryto_iner/services/http_service.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
@@ -10,8 +14,60 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage> {
+  double? _deviceHeight, _deviceWidth; //设备高度和宽度 or Device height and width
+
+  HTTPService? _http;
+  @override
+  void initState() {
+    super.initState();
+    _http = GetIt.instance.get<HTTPService>();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [_selectedCoinDropdown()],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _selectedCoinDropdown() {
+    List<String> _coins = ["bitcoin"];
+    List<DropdownMenuItem<String>> _items = _coins
+        .map((e) => DropdownMenuItem(
+              value: e,
+              child: Text(
+                e,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ))
+        .toList();
+
+    return DropdownButton(
+      value: _coins.first,
+      items: _items,
+      onChanged: (_value) {},
+      dropdownColor: const Color.fromRGBO(255, 255, 255, 1.0),
+      iconSize: 30,
+      icon: const Icon(
+        Icons.arrow_circle_down_sharp,
+        color: Colors.black,
+      ),
+      underline: Container(),
+    );
   }
 }
