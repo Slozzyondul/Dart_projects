089c6a60c73d3c01e8a3cd31b369dba87659920f
Slozzyondul <solomonondula@gmail.com>
2024-07-03 15:33:04 +0300

    addining the login button to the UI

--- flutter_projects/ondulgram/ondulgram/lib/pages/login_page.dart
+++ flutter_projects/ondulgram/ondulgram/lib/pages/login_page.dart
@@ -10,11 +10,58 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  double? _deviceHeight, _deviceWidth;
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        color: Colors.yellow,
+      backgroundColor: Colors.yellow,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth! * 0.05,
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _titleWidget(),
+                _loginButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return const Text(
+      "ondulgram",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return MaterialButton(
+      onPressed: () {},
+      minWidth: _deviceWidth! * 0.7,
+      height: _deviceHeight! * 0.06,
+      color: Colors.black,
+      child: const Text(
+        "login",
+        style: TextStyle(
+          color: Colors.yellow,
+          fontSize: 25,
+          fontWeight: FontWeight.w600,
+        ),
       ),
     );
   }
