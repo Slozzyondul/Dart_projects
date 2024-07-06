66219ec38ffe65949278662bbd3508ebe9506e11
Slozzyondul <solomonondula@gmail.com>
2024-07-06 17:11:51 +0300

    displaying the user profile picture from firebase to the app UI

--- flutter_projects/ondulgram/ondulgram/lib/pages/profile_page.dart
+++ flutter_projects/ondulgram/ondulgram/lib/pages/profile_page.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:ondulgram/services/firebase_service.dart';
 
-class ProfilePage extends StatefulWidget{
+class ProfilePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _ProfilePageState();
@@ -8,10 +11,49 @@ class ProfilePage extends StatefulWidget{
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  double? _deviceHeight, _deviceWidth;
+  FirebaseService? _firebaseService;
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseService = GetIt.instance.get<FirebaseService>();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Container(
-      color: Colors.yellow,
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth! * 0.05,
+        vertical: _deviceHeight! * 0.02,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _profileImage(),
+        ],
+      ),
+    );
+  }
+
+  Widget _profileImage(){
+    return Container(
+      margin: EdgeInsets.only(bottom: _deviceHeight! * 0.02),
+      height: _deviceHeight! * 0.15,
+      width: _deviceWidth! * 015,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            _firebaseService!.currentUser!["image"],
+          ),
+        ),
+      ),
     );
   }
 }
