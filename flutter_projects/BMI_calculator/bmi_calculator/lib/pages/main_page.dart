bf827408feea177f078c1c6e92c90a90742b0240
Slozzyondul <solomonondula@gmail.com>
2024-07-08 19:13:44 +0300

    adding cupertino tab scaffold and cupertino tab widget to switch between home and history pages

--- flutter_projects/BMI_calculator/bmi_calculator/lib/pages/main_page.dart
+++ flutter_projects/BMI_calculator/bmi_calculator/lib/pages/main_page.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/pages/bmi_page.dart';
+import 'package:bmi_calculator/pages/history_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,13 +11,38 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Widget> _tabs = [
+    BMIPage(),
+    HistoryPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(
         middle: Text('IBMI'),
       ),
-      child: Container(),
+      child: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: "BMI",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person),
+              label: "History",
+            ),
+          ],
+        ),
+        tabBuilder: (BuildContext context, int index) {
+          return CupertinoTabView(
+            builder: (BuildContext context) {
+              return _tabs[index];
+            },
+          );
+        },
+      ),
     );
   }
 }
