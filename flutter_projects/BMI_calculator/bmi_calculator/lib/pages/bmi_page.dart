aa31de237eba68e6da74f050ec1672aff651e926
Slozzyondul <solomonondula@gmail.com>
2024-07-09 15:41:51 +0300

    adding shadow box inthe app

--- flutter_projects/BMI_calculator/bmi_calculator/lib/pages/bmi_page.dart
+++ flutter_projects/BMI_calculator/bmi_calculator/lib/pages/bmi_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/widgets/info_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,11 +10,22 @@ class BMIPage extends StatefulWidget {
 }
 
 class _BMIPageState extends State<BMIPage> {
+  double? _deviceHeight, _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return CupertinoPageScaffold(
       child: Container(
         color: Colors.black,
+        child: Center(
+          child: InfoCard(
+            height: _deviceHeight! * 0.2,
+            width: _deviceWidth! * 0.45,
+            child: Container(),
+          ),
+        ),
       ),
     );
   }
---
+++ flutter_projects/BMI_calculator/bmi_calculator/lib/widgets/info_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InfoCard extends StatelessWidget {
+  final Widget child;
+  final double width, height;
+
+  const InfoCard({
+    required this.height,
+    required this.width,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: const BoxDecoration(
+        color: Colors.yellowAccent,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.yellowAccent,
+            //spreadRadius: 2,
+            blurRadius: 5,
+            //offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
