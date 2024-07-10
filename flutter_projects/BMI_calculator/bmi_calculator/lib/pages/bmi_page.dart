d9dd9d26ca0157db727cde8f66ab07c608890624
Slozzyondul <solomonondula@gmail.com>
2024-07-10 15:53:07 +0300

    adding a weight tracking widget

--- flutter_projects/BMI_calculator/bmi_calculator/lib/pages/bmi_page.dart
+++ flutter_projects/BMI_calculator/bmi_calculator/lib/pages/bmi_page.dart
@@ -12,6 +12,7 @@ class BMIPage extends StatefulWidget {
 class _BMIPageState extends State<BMIPage> {
   double? _deviceHeight, _deviceWidth;
   int _age = 27;
+  int _weight = 160;
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +20,22 @@ class _BMIPageState extends State<BMIPage> {
     _deviceWidth = MediaQuery.of(context).size.width;
     return CupertinoPageScaffold(
       child: Container(
-        color: Colors.black,
-        child: Center(
-          child: _ageSelectContainer(),
+        height: _deviceHeight! * 0.85,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _ageSelectContainer(),
+                _weightSelectorContainer(),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -43,7 +57,7 @@ class _BMIPageState extends State<BMIPage> {
             ),
           ),
           Text(
-            _age.toString(),
+            _weight.toString(),
             style: TextStyle(
               fontSize: 45,
               fontWeight: FontWeight.w700,
@@ -78,6 +92,74 @@ class _BMIPageState extends State<BMIPage> {
                     setState(
                       () {
                         _age++;
+                      },
+                    );
+                  },
+                  child: Text('+'),
+                  textStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _weightSelectorContainer() {
+    return InfoCard(
+      height: _deviceHeight! * 0.2,
+      width: _deviceWidth! * 0.45,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'weight lbs',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          Text(
+            _weight.toString(),
+            style: TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 50,
+                child: CupertinoDialogAction(
+                  onPressed: () {
+                    setState(
+                      () {
+                        _weight--;
+                      },
+                    );
+                  },
+                  child: Text('-'),
+                  textStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 50,
+                child: CupertinoDialogAction(
+                  onPressed: () {
+                    setState(
+                      () {
+                        _weight++;
                       },
                     );
                   },
