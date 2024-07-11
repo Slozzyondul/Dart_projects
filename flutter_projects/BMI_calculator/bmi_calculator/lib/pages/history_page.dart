59b5cca7c7d171bb5447984f0428f1c2bbac0218
Slozzyondul <solomonondula@gmail.com>
2024-07-11 10:43:13 +0300

    adding functionalities to retrieve the stored data in the shared preferences then display it on the history page of UI

--- flutter_projects/BMI_calculator/bmi_calculator/lib/pages/history_page.dart
+++ flutter_projects/BMI_calculator/bmi_calculator/lib/pages/history_page.dart
@@ -1,5 +1,7 @@
 import 'package:bmi_calculator/widgets/info_card.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HistoryPage extends StatelessWidget {
   double? _deviceHeight, _deviceWidth;
@@ -8,27 +10,54 @@ class HistoryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+
     return CupertinoPageScaffold(
       child: _dataCard(),
     );
   }
 
   Widget _dataCard() {
-    return Center(
-      child: InfoCard(
-        height: _deviceHeight! * 0.25,
-        width: _deviceWidth! * 0.75,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _statusText('normal'),
-            _dateText("11/07/2022"),
-            _bmiText("23.8")
-          ],
-        ),
-      ),
+    return FutureBuilder(
+      future: SharedPreferences.getInstance(),
+      builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+        if (_snapshot.hasData) {
+          final _prefs = _snapshot.data as SharedPreferences;
+          final _date = _prefs.getString('bmi_date');
+          final _dataString = _prefs.getString('bmi_data');
+
+          if (_date != null && _dataString != null) {
+            // Split the string to get the list
+            List<String> _data = _dataString.split(',');
+
+            return Center(
+              child: InfoCard(
+                height: _deviceHeight! * 0.25,
+                width: _deviceWidth! * 0.75,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    _statusText(_data[1]),
+                    _dateText(_date),
+                    _bmiText(_data[0])
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return Center(
+              child: Text('No BMI data found'),
+            );
+          }
+        } else {
+          return Center(
+            child: CupertinoActivityIndicator(
+              color: Colors.yellow,
+            ),
+          );
+        }
+      },
     );
   }
 
@@ -43,8 +72,9 @@ class HistoryPage extends StatelessWidget {
   }
 
   Widget _dateText(String _date) {
+    DateTime _parsedDate = DateTime.parse(_date);
     return Text(
-      _date,
+      '${_parsedDate.day} / ${_parsedDate.month} / ${_parsedDate.year}',
       style: TextStyle(
         fontSize: 20,
         fontWeight: FontWeight.w400,
@@ -54,7 +84,7 @@ class HistoryPage extends StatelessWidget {
 
   Widget _bmiText(String _bmi) {
     return Text(
-      _bmi,
+      double.parse(_bmi).toStringAsFixed(2),
       style: TextStyle(
         fontSize: 60,
         fontWeight: FontWeight.w600,
