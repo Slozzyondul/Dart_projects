aa4ee5ff4ab1ffb37e1d44cb8baeeda6da8777c3
Slozzyondul <solomonondula@gmail.com>
2024-07-11 12:09:39 +0300

    dependancy mocking using mocktail

--- flutter_projects/BMI_calculator/bmi_calculator/test/utils/calculator_test.dart
+++ flutter_projects/BMI_calculator/bmi_calculator/test/utils/calculator_test.dart
@@ -1,8 +1,16 @@
+import 'dart:convert';
+
 import 'package:bmi_calculator/utils/calculator.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class DioMock extends Mock implements Dio {}
 
 void main() {
-  test('give height and weight when calculateBMI function called then correct BMI retured', () {
+  test(
+      'give height and weight when calculateBMI function called then correct BMI retured',
+      () {
     //arrange
     const int height = 70, weight = 160;
 
@@ -12,4 +20,27 @@ void main() {
     //assert
     expect(bmi, 22.955102040816328);
   });
+
+  test(
+    'given url when calculateBMIAsync invoked then correct bmi returned',
+    () async {
+      //arrange
+      final _dioMock = DioMock();
+      when(() => _dioMock.get('https://jsonkeeper.com/b/AKFA')).thenAnswer(
+        (_) => Future.value(
+          Response(
+            requestOptions:
+                RequestOptions(path: 'https://jsonkeeper.com/b/AKFA'),
+            data: {
+              "Sergio Ramos": [72, 165],
+            },
+          ),
+        ),
+      );
+      //act
+      var _result = await calculateBMIAsync(_dioMock);
+      //assert
+      expect(_result, 22.375578703703706);
+    },
+  );
 }
