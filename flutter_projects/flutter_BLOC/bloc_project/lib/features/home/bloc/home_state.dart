8371ea07be76c7fec91ef6020896200271c3d5ff
Slozzyondul <solomonondula@gmail.com>
2024-07-18 17:41:02 +0300

    adding possible states in the home bloc class

--- flutter_projects/flutter_BLOC/bloc_project/lib/features/home/bloc/home_state.dart
+++ flutter_projects/flutter_BLOC/bloc_project/lib/features/home/bloc/home_state.dart
@@ -3,4 +3,16 @@ part of 'home_bloc.dart';
 @immutable
 sealed class HomeState {}
 
+abstract class HomeActionState extends HomeState {}
+
 final class HomeInitial extends HomeState {}
+
+final class HomeLoading extends HomeState {}
+
+class HomeLoadedSuccessState extends HomeState {}
+
+class HomeErrorState extends HomeState {}
+
+class HomeNavigateToWishlistPageActionState extends HomeState {}
+
+class HomeNavigateToCartPageActionState extends HomeState {}
