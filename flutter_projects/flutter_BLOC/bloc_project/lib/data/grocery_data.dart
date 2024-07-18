34570ec25c63f443f572b155ad0968af00008367
Slozzyondul <solomonondula@gmail.com>
2024-07-18 17:13:27 +0300

    restyling the app structure

--- flutter_projects/flutter_BLOC/bloc_project/lib/data/grocery_data.dart
+++ flutter_projects/flutter_BLOC/bloc_project/lib/data/grocery_data.dart
@@ -1,39 +1,44 @@
 class GroceryData {
-  List<Map<String, dynamic>> groceryProducts = [
+  static List<Map<String, dynamic>> groceryProducts = [
     {
       'id': 1,
       'name': 'Apples',
       'description': 'Fresh and juicy apples.',
       'price': 1.2,
-      'imageUrl': 'https://example.com/images/apples.jpg',
+      'imageUrl':
+          'https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=400',
     },
     {
       'id': 2,
       'name': 'Bananas',
       'description': 'Ripe and sweet bananas.',
       'price': 0.5,
-      'imageUrl': 'https://example.com/images/bananas.jpg',
+      'imageUrl':
+          'https://images.pexels.com/photos/1093038/pexels-photo-1093038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
       'id': 3,
       'name': 'Bread',
       'description': 'Whole grain bread.',
       'price': 2.5,
-      'imageUrl': 'https://example.com/images/bread.jpg',
+      'imageUrl':
+          'https://images.pexels.com/photos/960662/pexels-photo-960662.jpeg',
     },
     {
       'id': 4,
       'name': 'Milk',
       'description': '1 liter of fresh milk.',
       'price': 1.0,
-      'imageUrl': 'https://example.com/images/milk.jpg',
+      'imageUrl':
+          'https://images.pexels.com/photos/5946717/pexels-photo-5946717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
       'id': 5,
       'name': 'Eggs',
       'description': 'A dozen organic eggs.',
       'price': 3.0,
-      'imageUrl': 'https://example.com/images/eggs.jpg',
+      'imageUrl':
+          'https://images.pexels.com/photos/4045561/pexels-photo-4045561.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
   ];
 }
--- flutter_projects/flutter_BLOC/bloc_project/lib/main.dart
+++ flutter_projects/flutter_BLOC/bloc_project/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_project/features/home/ui/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,22 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellowAccent),
         useMaterial3: true,
       ),
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.grey,
-          ),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
