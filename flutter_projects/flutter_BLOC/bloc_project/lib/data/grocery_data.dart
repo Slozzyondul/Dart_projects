9b8007cd825fa0baba513f8960c35fa3846ade5d
Slozzyondul <solomonondula@gmail.com>
2024-07-18 16:44:57 +0300

    adding grocery data

---
+++ flutter_projects/flutter_BLOC/bloc_project/lib/data/grocery_data.dart
@@ -0,0 +1,39 @@
+class GroceryData {
+  List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': 1,
+      'name': 'Apples',
+      'description': 'Fresh and juicy apples.',
+      'price': 1.2,
+      'imageUrl': 'https://example.com/images/apples.jpg',
+    },
+    {
+      'id': 2,
+      'name': 'Bananas',
+      'description': 'Ripe and sweet bananas.',
+      'price': 0.5,
+      'imageUrl': 'https://example.com/images/bananas.jpg',
+    },
+    {
+      'id': 3,
+      'name': 'Bread',
+      'description': 'Whole grain bread.',
+      'price': 2.5,
+      'imageUrl': 'https://example.com/images/bread.jpg',
+    },
+    {
+      'id': 4,
+      'name': 'Milk',
+      'description': '1 liter of fresh milk.',
+      'price': 1.0,
+      'imageUrl': 'https://example.com/images/milk.jpg',
+    },
+    {
+      'id': 5,
+      'name': 'Eggs',
+      'description': 'A dozen organic eggs.',
+      'price': 3.0,
+      'imageUrl': 'https://example.com/images/eggs.jpg',
+    },
+  ];
+}
--- flutter_projects/flutter_BLOC/bloc_project/lib/main.dart
+++ flutter_projects/flutter_BLOC/bloc_project/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            backgroundColor: Colors.black,
+            backgroundColor: Colors.grey,
           ),
         ),
       ),
