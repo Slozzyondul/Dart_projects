689c92747e54a01dd5a44eed67ad511807ab9ecf
Slozzyondul <solomonondula@gmail.com>
2024-07-18 18:48:53 +0300

    adding event listeners methods to both the cart and wishlist

--- flutter_projects/flutter_BLOC/bloc_project/lib/main.dart
+++ flutter_projects/flutter_BLOC/bloc_project/lib/main.dart
@@ -12,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellowAccent),
-        useMaterial3: true,
-      ),
+      theme: ThemeData(primaryColor: Colors.tealAccent),
       home: const Home(),
     );
   }
