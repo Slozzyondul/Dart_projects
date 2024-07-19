d14f3b8ebfd657c9d22b6d9dd0a60884294cd9af
Slozzyondul <solomonondula@gmail.com>
2024-07-19 11:42:49 +0300

    adding listview to display products in the homepage

---
+++ flutter_projects/flutter_BLOC/bloc_project/lib/features/home/ui/product_tile_widget.dart
@@ -0,0 +1,25 @@
+import 'package:bloc_project/features/home/models/home_product_data_model.dart';
+import 'package:flutter/material.dart';
+
+class ProductTileWidget extends StatelessWidget {
+  final ProductDataModel productDataModel;
+  const ProductTileWidget({super.key, required this.productDataModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Container(
+            height: 200,
+            width: double.maxFinite,
+          ),
+          Text(productDataModel.name),
+          Text(productDataModel.description),
+        ],
+      ),
+    );
+  }
+}
