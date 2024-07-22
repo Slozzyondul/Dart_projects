ca5c39d68067b82ad0be2bcea36c9812a7af0f27
Slozzyondul <solomonondula@gmail.com>
2024-07-22 15:26:32 +0300

    setting wshlist page initial status event

--- flutter_projects/flutter_BLOC/bloc_project/lib/features/wishlist/ui/wishlist.dart
+++ flutter_projects/flutter_BLOC/bloc_project/lib/features/wishlist/ui/wishlist.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_project/features/wishlist/bloc/wishlist_bloc.dart';
 import 'package:flutter/material.dart';
 
 class Wishlist extends StatefulWidget {
@@ -8,8 +9,23 @@ class Wishlist extends StatefulWidget {
 }
 
 class _WishlistState extends State<Wishlist> {
+  final WishlistBloc wishlistBloc = WishlistBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    wishlistBloc.add(WishlistInitialEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("wishlist items"),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
