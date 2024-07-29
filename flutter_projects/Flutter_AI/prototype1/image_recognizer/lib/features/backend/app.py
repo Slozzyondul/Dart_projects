1d93b0724e67b027377630f6334ce401dd42c45b
Slozzyondul <solomonondula@gmail.com>
2024-07-29 16:23:55 +0300

    adjusting app.py t start local server for testing image uploads

--- flutter_projects/Flutter_AI/prototype1/image_recognizer/lib/features/backend/app.py
+++ flutter_projects/Flutter_AI/prototype1/image_recognizer/lib/features/backend/app.py
@@ -9,68 +9,68 @@ UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Load YOLO
-net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+# net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+# layer_names = net.getLayerNames()
+# output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-# Load coco.names
-with open("coco.names", "r") as f:
-    classes = [line.strip() for line in f.readlines()]
+# # Load coco.names
+# with open("coco.names", "r") as f:
+#     classes = [line.strip() for line in f.readlines()]
 
-def detect_objects(image_path):
-    image = cv2.imread(image_path)
-    height, width, channels = image.shape
+# def detect_objects(image_path):
+#     image = cv2.imread(image_path)
+#     height, width, channels = image.shape
 
-    # Detecting objects
-    blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    net.setInput(blob)
-    outs = net.forward(output_layers)
+#     # Detecting objects
+#     blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+#     net.setInput(blob)
+#     outs = net.forward(output_layers)
 
-    class_ids = []
-    confidences = []
-    boxes = []
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                # Object detected
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
+#     class_ids = []
+#     confidences = []
+#     boxes = []
+#     for out in outs:
+#         for detection in out:
+#             scores = detection[5:]
+#             class_id = np.argmax(scores)
+#             confidence = scores[class_id]
+#             if confidence > 0.5:
+#                 # Object detected
+#                 center_x = int(detection[0] * width)
+#                 center_y = int(detection[1] * height)
+#                 w = int(detection[2] * width)
+#                 h = int(detection[3] * height)
 
-                # Rectangle coordinates
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
+#                 # Rectangle coordinates
+#                 x = int(center_x - w / 2)
+#                 y = int(center_y - h / 2)
 
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
+#                 boxes.append([x, y, w, h])
+#                 confidences.append(float(confidence))
+#                 class_ids.append(class_id)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    detected_objects = []
-    for i in range(len(boxes)):
-        if i in indexes:
-            label = str(classes[class_ids[i]])
-            detected_objects.append(label)
+#     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+#     detected_objects = []
+#     for i in range(len(boxes)):
+#         if i in indexes:
+#             label = str(classes[class_ids[i]])
+#             detected_objects.append(label)
 
-    return detected_objects
+#     return detected_objects
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return "No file part"
-    file = request.files['file']
-    if file.filename == '':
-        return "No selected file"
-    if file:
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        detected_objects = detect_objects(file_path)
-        return jsonify({"filename": filename, "detected_objects": detected_objects})
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     if 'file' not in request.files:
+#         return "No file part"
+#     file = request.files['file']
+#     if file.filename == '':
+#         return "No selected file"
+#     if file:
+#         filename = secure_filename(file.filename)
+#         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#         file.save(file_path)
+#         detected_objects = detect_objects(file_path)
+#         return jsonify({"filename": filename, "detected_objects": detected_objects})
 
 @app.route('/search', methods=['GET'])
 def search_objects():
