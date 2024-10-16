62567717fd090e65daa416683dfdb0a171de3686
Slozzyondul <solomonondula@gmail.com>
2024-10-16 21:03:19 +0300

    using  firebase auth ui to come up with custom profile page with less code

--- flutter_projects/flutter_firebase/lib/src/screens/custom_sign_in_screen.dart
+++ flutter_projects/flutter_firebase/lib/src/screens/custom_sign_in_screen.dart
@@ -1,19 +1,34 @@
+import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authtProviders = [EmailAuthProvider()];
+    final authtProviders = <AuthProvider>[
+      EmailAuthProvider(),
+      //PhoneAuthProvider()
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign in'),
       ),
       body: SignInScreen(
+        
         providers: authtProviders,
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          }),
+
+          AuthStateChangeAction<UserCreated>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          })
+        ],
       ),
     );
   }
