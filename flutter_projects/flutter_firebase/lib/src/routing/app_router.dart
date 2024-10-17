7faee6ca2d789316abbd0acb4e2324bb94118a2b
Slozzyondul <solomonondula@gmail.com>
2024-10-17 18:06:53 +0300

    adding home route to gorouter and updating redirect logic

--- flutter_projects/flutter_firebase/lib/src/routing/app_router.dart
+++ flutter_projects/flutter_firebase/lib/src/routing/app_router.dart
@@ -1,40 +1,41 @@
 import 'package:faker_app_flutter_firebase/src/routing/go_router_refresh_stream.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_profile_screen.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_sign_in_screen.dart';
+import 'package:faker_app_flutter_firebase/src/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 enum AppRoute {
   signIn,
+  home,
   profile,
 }
 
-final FirebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
   return FirebaseAuth.instance;
 });
 
 final goRouterProvider = Provider<GoRouter>((ref) {
-  final firebaseAuth = ref.watch(FirebaseAuthProvider);
+  final firebaseAuth = ref.watch(firebaseAuthProvider);
   return GoRouter(
     initialLocation: '/sign-in',
     debugLogDiagnostics: true,
     redirect: (context, state) {
       final isLoggedIn = firebaseAuth.currentUser != null;
-      
-      // If user is not logged in and trying to access profile, redirect to sign-in
-      if (!isLoggedIn && state.location == '/profile') {
-        return '/sign-in';
-      }
-      
-      // If user is logged in and trying to access sign-in, redirect to profile
-      if (isLoggedIn && state.location == '/sign-in') {
-        return '/profile';
-      }
-      
-      return null; // No redirection needed
-    },
 
+      if (isLoggedIn) {
+        if (state.location == '/sign-in') {
+          return '/home';
+        }
+      } else {
+        if (state.location.startsWith('/home')) {
+          return '/sign-in';
+        }
+      }
+
+      return null;
+    },
     refreshListenable: GoRouterRefreshStream(firebaseAuth.authStateChanges()),
     routes: [
       GoRoute(
@@ -43,9 +44,16 @@ final goRouterProvider = Provider<GoRouter>((ref) {
         builder: (context, state) => const CustomSignInScreen(),
       ),
       GoRoute(
-        path: '/profile',
-        name: AppRoute.profile.name,
-        builder: (context, state) => const CustomProfileScreen(),
+        path: '/home',
+        name: AppRoute.home.name,
+        builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            path: 'profile',
+            name: AppRoute.profile.name,
+            builder: (context, state) => const CustomProfileScreen(),
+          ),
+        ],
       ),
     ],
   );
