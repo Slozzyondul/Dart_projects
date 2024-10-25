5929269fe1f5e3fafd55dbda64fe77f09ef1988f
Slozzyondul <solomonondula@gmail.com>
2024-10-25 11:27:34 +0300

    image upload to firebase storage from app

---
+++ flutter_projects/shopping_app_with_firebase_image_upload/ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/products_admin/data/image_upload_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'admin_product_upload_controller.g.dart';
+
+@riverpod
+class AdminProductUploadController extends _$AdminProductUploadController {
+  @override
+  FutureOr<void> build() {}
+
+  Future<void> upload(Product product) async {
+    try {
+      state = const AsyncLoading();
+
+      final downloadUrl = await ref
+          .read(imageUploadRepositoryProvider)
+          .uploadProductImageFromAsset(
+            product.imageUrl,
+            product.id,
+          );
+          // TODO: save downloadUrl to firestore 
+      state = const AsyncData(null);
+      // TODO: on succes move to the edit product page
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
+  }
+}
