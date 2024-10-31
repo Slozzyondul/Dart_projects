637e1e2731cf8694f3e14a49103ac4d02be91512
Slozzyondul <solomonondula@gmail.com>
2024-10-31 17:06:48 +0300

    setting up more linter rules

--- flutter_projects/firebase cloud functions/functions/.eslintrc.js
+++ flutter_projects/firebase cloud functions/functions/.eslintrc.js
@@ -29,5 +29,13 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "semi": ["off"],
+    "require-jsdoc": ["off"],
+    "object-curly-spacing": ["off"],
+    "spaced-comment": ["off"],
+    "max-len": [
+      "error",
+      { "code": 140, },
+    ]
   },
 };
