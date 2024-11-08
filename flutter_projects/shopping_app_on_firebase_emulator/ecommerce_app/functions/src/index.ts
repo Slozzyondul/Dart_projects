22baf1ac1b1f3a401fea566baddbf575e11101d6
Slozzyondul <solomonondula@gmail.com>
2024-11-08 14:15:31 +0300

    adding custom claim on the server cloud to store a whitelist of admins

--- flutter_projects/shopping_app_on_firebase_emulator/ecommerce_app/functions/src/index.ts
+++ flutter_projects/shopping_app_on_firebase_emulator/ecommerce_app/functions/src/index.ts
@@ -1,19 +1,39 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as admin from "firebase-admin"
+//import * as functions from "firebase-functions"
+import * as logger from "firebase-functions/logger"
+import * as functions from 'firebase-functions/v1';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp()
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const makeAdminIfWhitelisted = functions.auth.user().onCreate(async (user, _) => {
+    const email = user.email
+
+    if (email === undefined) {
+        logger.log(`User ${user.uid} has no email`)
+        return
+    }
+    // white list of admin email list
+    const whitelisted = ["solomonondula@gmail.com"]
+
+    if (!whitelisted.includes(email)) {
+        return
+    }
+    //whitelist by email domain name
+    // if (!email.endsWith("@gmail.com")) {
+    //     logger.log(`user ${email} is not whitelisted`)
+    //     return
+    // }
+
+    if (user.customClaims?.admin === true) {
+        logger.log(`user ${email} is already admin`)
+        return
+    }
+
+
+    // set custom claim
+    await admin.auth().setCustomUserClaims(user.uid, {
+        admin: true,
+    })
+    logger.log(`custom claim ${email} is now admin`)
+})
