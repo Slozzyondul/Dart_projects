0a657ad35f6960b62384f53179b047a7497a84e7
Slozzyondul <solomonondula@gmail.com>
2025-04-25 12:20:27 +0300

    update

--- flutter_projects/flutter-integration-test-masterclass/integration_test/app_test.dart
+++ flutter_projects/flutter-integration-test-masterclass/integration_test/app_test.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_starter/main.dart' as app;
@@ -14,6 +17,9 @@ void main() {
 
       final widget = find.text("some text");
       expect(widget, findsNothing);
+
+      expect(find.byIcon(Icons.add), findsOneWidget);
+      expect(find.textContaining("login"), findsNWidgets(2));
     });
   });
 }
