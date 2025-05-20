9d3e74ad62771f71f4800d0475c9e9ea9aedfea2
slozzy <solomonondula@gmail.com>
2025-05-20 15:28:06 +0300

    initialize go router

--- flutter_projects/ecommerce_app/lib/src/app.dart
+++ flutter_projects/ecommerce_app/lib/src/app.dart
@@ -1,16 +1,17 @@
-import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final goRouter = GoRouter(routes: []);
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      routerConfig: null,
       restorationScopeId: 'app',
-      home: const ProductsListScreen(),
       onGenerateTitle: (BuildContext context) => 'My Shop'.hardcoded,
       theme: ThemeData(
         visualDensity: const VisualDensity(),
