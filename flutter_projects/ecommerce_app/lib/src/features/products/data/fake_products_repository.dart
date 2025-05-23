2a27bdab6be97b9d8d983f62e0a062cc8d44bb97
slozzy <solomonondula@gmail.com>
2025-05-23 10:43:24 +0300

    getting products grid content from new products repository

---
+++ flutter_projects/ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
@@ -0,0 +1,12 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+
+class FakeProductsRepository {
+  //private constructor
+  FakeProductsRepository._();
+  //making a singleton
+  static FakeProductsRepository instance = FakeProductsRepository._();
+  List<Product> getProductsList() {
+    return kTestProducts;
+  }
+}
