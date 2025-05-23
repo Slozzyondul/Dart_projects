1cfe3d6f3e1342035ba593e54e9e683cf6b84093
slozzy <solomonondula@gmail.com>
2025-05-23 10:59:35 +0300

    adding stream and future methods to work with real api in the fake repository

--- flutter_projects/ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
+++ flutter_projects/ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
@@ -6,11 +6,21 @@ class FakeProductsRepository {
   FakeProductsRepository._();
   //making a singleton
   static FakeProductsRepository instance = FakeProductsRepository._();
+  final List<Product> _products = kTestProducts;
+
   List<Product> getProductsList() {
-    return kTestProducts;
+    return _products;
   }
 
   Product? getProduct(String id) {
-    return kTestProducts.firstWhere((product) => product.id == id);
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  Future<List<Product>> fetchProductsList() {
+    return Future.value(_products);
+  }
+
+  Stream<List<Product>> watchProductsList() {
+    return Stream.value(_products);
   }
 }
