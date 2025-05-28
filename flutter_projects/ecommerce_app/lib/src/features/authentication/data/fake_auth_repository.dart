0e9334811852fb331e61cb654f812c13ab275da9
Slozzyondul <solomonondula@gmail.com>
2025-05-28 18:06:58 +0300

    undo changes to only work with fake repository until firebase is setup

--- flutter_projects/ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ flutter_projects/ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,61 +1,24 @@
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-abstract class AuthRepository {
-  Stream<AppUser?> authStateChanges();
-  AppUser? get currentUser;
-  Future<void> signInWithEmailAndPassword(String email, String password);
-  Future<void> createUserWithEmailAndPassword(String email, String password);
-  Future<void> signOut();
-}
-
-class FirebaseAuthRepository implements AuthRepository {
-  @override
-  Stream<AppUser?> authStateChanges() {
-    //TODO: Implement
-    throw UnimplementedError();
-  }
-
-  @override
-  AppUser? get currentUser => null; //TODO: Implement
-  @override
-  Future<void> signInWithEmailAndPassword(String email, String password) async {
-    //TODO: Implement
-  }
-  @override
-  Future<void> createUserWithEmailAndPassword(
-      String email, String password) async {
-    //TODO: Implement
-  }
-  @override
-  Future<void> signOut() async {
-    //TODO: Implement
-  }
-}
-
-class FakeAuthRepository implements AuthRepository {
-  @override
+class FakeAuthRepository {
   Stream<AppUser?> authStateChanges() => Stream.value(null); //TODO: Implement
-  @override
   AppUser? get currentUser => null; //TODO: Implement
-  @override
+
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     //TODO: Implement
   }
-  @override
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
     //TODO: Implement
   }
-  @override
   Future<void> signOut() async {
     //TODO: Implement
   }
 }
 
-final authRepositoryProvider = Provider<AuthRepository>((ref) {
-  final isFake = String.fromEnvironment('useFakeRepos') == 'true';
-  return isFake ? FakeAuthRepository() : FirebaseAuthRepository();
+final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
+  return FakeAuthRepository();
 });
 
 final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
