899817a190ba3f289c7a2cb3e8550f3b4184f30e
Slozzyondul <solomonondula@gmail.com>
2025-05-29 10:40:14 +0300

    disposing the fake auth repo in the provider

--- flutter_projects/ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ flutter_projects/ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -34,7 +34,9 @@ class FakeAuthRepository {
 }
 
 final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
-  return FakeAuthRepository();
+  final auth = FakeAuthRepository();
+  ref.onDispose(() => auth.dispose());
+  return auth;
 });
 
 final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
