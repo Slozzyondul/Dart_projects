effe6cfe1b50d600787df19a0b664461fe435b41
Slozzyondul <solomonondula@gmail.com>
2025-05-29 10:31:32 +0300

    implementing signin method with emaila and reversed email as uid in fake auth repo

--- flutter_projects/ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ flutter_projects/ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,13 +1,21 @@
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeAuthRepository {
-  Stream<AppUser?> authStateChanges() => Stream.value(null); //TODO: Implement
-  AppUser? get currentUser => null; //TODO: Implement
+  final _authState = InMemoryStore<AppUser?>(null);
+  Stream<AppUser?> authStateChanges() => _authState.stream;
+  AppUser? get currentUser => _authState.value;
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
-    //TODO: Implement
+    if (currentUser == null) {
+      _authState.value = AppUser(
+        uid: email.split('').reversed.join(),
+        email: email,
+      );
+    }
   }
+
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
     //TODO: Implement
