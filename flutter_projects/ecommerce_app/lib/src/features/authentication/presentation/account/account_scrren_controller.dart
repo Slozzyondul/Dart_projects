d6bba6cca0998cc6a81bbb707b6ce39b6a43d0df
Slozzyondul <solomonondula@gmail.com>
2025-05-29 11:53:53 +0300

    account screen controller with all the possible states(loading, data and error)

--- flutter_projects/ecommerce_app/lib/src/features/authentication/presentation/account/account_scrren_controller.dart
+++ flutter_projects/ecommerce_app/lib/src/features/authentication/presentation/account/account_scrren_controller.dart
@@ -11,5 +11,12 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
     // sign out using auth repository
     // if success, set state to data
     // if error,set state to error
+    try {
+      state = const AsyncValue<void>.loading();
+      await authRepository.signOut();
+      state = const AsyncValue<void>.data(null);
+    } catch (e, st) {
+      state = AsyncValue<void>.error(e, st);
+    }
   }
 }
