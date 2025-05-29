9a4a72e6bb4a9f2a4d4533d9970881549e9c4de3
Slozzyondul <solomonondula@gmail.com>
2025-05-29 11:47:16 +0300

    adding account screen controller to manage account widget state

---
+++ flutter_projects/ecommerce_app/lib/src/features/authentication/presentation/account/account_scrren_controller.dart
@@ -0,0 +1,15 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+  AccountScreenController({required this.authRepository})
+      : super(const AsyncValue<void>.data(null));
+  final FakeAuthRepository authRepository;
+
+  Future<void> signOut() async {
+    // set state to loading
+    // sign out using auth repository
+    // if success, set state to data
+    // if error,set state to error
+  }
+}
