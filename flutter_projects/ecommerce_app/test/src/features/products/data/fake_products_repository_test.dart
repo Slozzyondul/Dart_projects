ff48830534ada33c1217a0e3e0886cc496853d8c
Slozzyondul <solomonondula@gmail.com>
2025-06-05 19:25:17 +0300

    grouping tests for products lists and getproducts

--- flutter_projects/ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
+++ flutter_projects/ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -3,28 +3,30 @@ import 'package:ecommerce_app/src/features/products/data/fake_products_repositor
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('getProductsList returns a global list of products', () {
-    final productsRepository = FakeProductsRepository();
-    final productsList = productsRepository.getProductsList();
-    expect(productsList, kTestProducts);
-  });
+  group('FakeProductsRepository', () {
+    test('getProductsList returns a global list of products', () {
+      final productsRepository = FakeProductsRepository();
+      final productsList = productsRepository.getProductsList();
+      expect(productsList, kTestProducts);
+    });
 
-  //retun non null product matching id
-  test('getProduct(1) returns a product matching id 1', () {
-    final productsRepository = FakeProductsRepository();
-    final product = productsRepository.getProduct('1');
-    expect(product, kTestProducts[0]);
-  });
+    //retun non null product matching id
+    test('getProduct(1) returns a product matching id 1', () {
+      final productsRepository = FakeProductsRepository();
+      final product = productsRepository.getProduct('1');
+      expect(product, kTestProducts[0]);
+    });
 
-  // return a null test an id that is not in the product list
-  test('getProduct(99) returns null', () {
-    final productsRepository = FakeProductsRepository();
-    final product = productsRepository.getProduct('99');
-    expect(
-      product,
-      null,
-      reason:
-          'getProduct(99) should return null because the id is not in the list',
-    );
+    // return a null test an id that is not in the product list
+    test('getProduct(99) returns null', () {
+      final productsRepository = FakeProductsRepository();
+      final product = productsRepository.getProduct('99');
+      expect(
+        product,
+        null,
+        reason:
+            'getProduct(99) should return null because the id is not in the list',
+      );
+    });
   });
 }
