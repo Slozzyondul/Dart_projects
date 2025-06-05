753dbc97d74d80876ecfa317a0328c79912923a0
Slozzyondul <solomonondula@gmail.com>
2025-06-05 18:16:53 +0300

    unit test to get products

--- flutter_projects/ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
+++ flutter_projects/ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -1,7 +1,11 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('fake products repository ...', (tester) async {
-    // TODO: Implement test
+  test('getProductsList returns a global list of products', () {
+    final productsRepository = FakeProductsRepository();
+    final productsList = productsRepository.getProductsList();
+    expect(productsList, kTestProducts);
   });
 }
