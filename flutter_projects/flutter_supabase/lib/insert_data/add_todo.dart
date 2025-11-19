dd247797bb0e36a7f40cfc5e853da5c4a969c4f3
Slozzyondul <solomonondula@gmail.com>
2025-11-19 12:13:22 +0300

    update text editing controllers

--- flutter_projects/flutter_supabase/lib/insert_data/add_todo.dart
+++ flutter_projects/flutter_supabase/lib/insert_data/add_todo.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
-class AddTodo extends StatelessWidget {
+class AddTodo extends StatefulWidget {
   const AddTodo({super.key});
 
   @override
+  State<AddTodo> createState() => _AddTodoState();
+}
+
+class _AddTodoState extends State<AddTodo> {
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Add Todo'), centerTitle: true));
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Todo'), centerTitle: true),
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: .center,
+          crossAxisAlignment: .center,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(hintText: 'Enter Todo Name'),
+            ),
+            SizedBox(height: 8),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(hintText: 'Enter Todo Description'),
+            ),
+            SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
   }
 }
