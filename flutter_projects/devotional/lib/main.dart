7ea37502bb1be6c4a8c0c2937b22d2c2202ae64e
Slozzyondul <solomonondula@gmail.com>
2026-01-13 19:27:35 +0300

    routing the app

--- flutter_projects/devotional/lib/main.dart
+++ flutter_projects/devotional/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:devotional/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
@@ -8,5 +9,5 @@ void main() {
   setUrlStrategy(PathUrlStrategy());
   WidgetsFlutterBinding.ensureInitialized();
   GoRouter.optionURLReflectsImperativeAPIs = true;
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
--- flutter_projects/devotional/lib/src/app.dart
+++ flutter_projects/devotional/lib/src/app.dart
@@ -1,14 +1,16 @@
 import 'package:devotional/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
     return MaterialApp.router(
-      routerConfig: AppRouter.routerConfig,
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
       restorationScopeId: 'app',
       title: 'Lilly Bossek',
