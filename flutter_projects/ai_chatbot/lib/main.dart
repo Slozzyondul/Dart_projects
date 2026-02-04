16ab5cde08acac3543c5a9b356a8d601ce45f8cc
Slozzyondul <solomonondula@gmail.com>
2026-02-04 12:09:53 +0300

    prompt screen

--- flutter_projects/ai_chatbot/lib/main.dart
+++ flutter_projects/ai_chatbot/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'ai chatbot',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const HomePage(),
     );
--- flutter_projects/ai_chatbot/lib/src/home_page.dart
+++ flutter_projects/ai_chatbot/lib/src/home_page.dart
@@ -10,6 +10,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.yellow, elevation: 16),
+      body: Column(
+        children: [
+          Expanded(child: Center(child: Text('Results from ai chatbot'))),
+          Row(
+            children: [
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(hintText: 'Enter your message'),
+                  ),
+                ),
+              ),
+              IconButton(icon: Icon(Icons.send), onPressed: () {}),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
